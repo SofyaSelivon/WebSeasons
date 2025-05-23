91e8d76f4a7014781aafe3d0d609eec8c2e74b0f
Sofya <si.selivon@yandex.ru>
2025-05-23 10:43:55 +0300

    urls

--- auth_service/users/urls.py
+++ auth_service/users/urls.py
@@ -1,7 +1,11 @@
+from django.http import JsonResponse
 from django.urls import path
 from . import views
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path("healthz/", health_check),
     path('register/', views.register_user),
     path('login/', views.login_user),
     path('logout/', views.logout_user),
