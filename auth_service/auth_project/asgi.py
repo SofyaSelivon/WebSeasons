d115d240ba705a215ac9b1feb7ba1d7594516640
Sofya <si.selivon@yandex.ru>
2025-05-23 13:38:17 +0300

    auth

--- auth_service/auth_project/asgi.py
+++ auth_service/auth_project/asgi.py
@@ -1,6 +1,10 @@
 import os
 from django.core.asgi import get_asgi_application
+import sys
+from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_project.settings')
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_service.auth_project.settings')
 
 application = get_asgi_application()
